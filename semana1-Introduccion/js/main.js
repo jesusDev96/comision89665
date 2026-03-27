a0a19c2e376ffd7c066516c876aa45e777fc2e64
jesusDev96 <chechujor@hotmail.com>
2026-03-26 22:20:52 -0300

    variables, constantes, prompt, console, alert. Semana 1

---
+++ semana1-Introduccion/js/main.js
@@ -0,0 +1,23 @@
+// let materia = "javascript"
+// let comision = 89665
+// let flex = true
+
+// alert("Materia: " + materia + " Comision N°" + comision)
+
+// materia = prompt("Ingrese la materia")
+// comision = prompt("Ingrese el numero de comision")
+
+
+// console.log("Materia: " + materia + " Comision N°" + comision)
+
+
+// let numeroA = parseInt(prompt("Ingrese el primer numero")) 
+// let numeroB = parseFloat(prompt("Ingrese el segundo numero")) 
+
+// let resultado = numeroA + numeroB
+
+// console.log(resultado)
+
+// const comision = 89665
+// comision = 95200
+// console.log(comision)
